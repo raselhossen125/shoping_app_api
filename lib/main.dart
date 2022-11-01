9e7204396d4ce8485d9c6e9c0507d341da4c54a7
rasel-hossen <straselhossen24@gmail.com>
2022-11-01 12:54:14 +0600

    design compleate

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'consts/global_colors.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter 3.0.4 ',
+      theme: ThemeData(
+        scaffoldBackgroundColor: lightScaffoldColor,
+        primaryColor: lightCardColor,
+        backgroundColor: lightBackgroundColor,
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(
+            color: lightIconsColor,
+          ),
+          backgroundColor: lightScaffoldColor,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+              color: lightTextColor, fontSize: 22, fontWeight: FontWeight.bold),
+          elevation: 0,
+        ),
+        iconTheme: IconThemeData(
+          color: lightIconsColor,
+        ),
+
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.black,
+          selectionColor: Colors.blue,
+          // selectionHandleColor: Colors.blue,
+        ),
+        // textTheme: TextTheme()
+        // textTheme: Theme.of(context).textTheme.apply(
+        //       bodyColor: Colors.black,
+        //       displayColor: Colors.black,
+        //     ),
+        cardColor: lightCardColor,
+        brightness: Brightness.light,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              secondary: lightIconsColor,
+              brightness: Brightness.light,
+            ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/users_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../widgets/users_widget.dart';
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Categories")),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (ctx, index) {
+              return const UsersWidget();
+            }));
+  }
+}
